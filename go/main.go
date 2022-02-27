5bde009b46d7ce2bdd643810b33b926d6a205b7b
Robin Bryce <robinbryce@gmail.com>
2022-02-27 13:57:51 +0000

    service skeleton

---
+++ go/main.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/robinbryce/apikeystore/server"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
+)
+
+const (
+	Name = "authex"
+)
+
+func main() {
+	v := viper.New()
+	cfg := server.NewConfig()
+	setDefaultConfig(v, Name, cfg)
+
+	f := pflag.NewFlagSet(Name, pflag.ContinueOnError)
+
+	var cfgFile string
+	f.StringVar(&cfgFile, "config", "", "configuration file. all options can be set in this")
+
+
+	f.StringVar(
+		&cfg.Mode, "mode", cfg.Mode, `
+	Mode to operate in. "reader" supports get/list & check. "writer" supports create/update/delete
+	`)
+
+	f.StringVar(
+		&cfg.Address, "address", cfg.Address, `
+	Listen address as host:port.
+	`)
+
+	f.StringVar(
+		&cfg.Prefix, "prefix", cfg.Prefix, `
+	Prefix all served routes with this value. A leading '/' is added to the path
+	if absent. You must include trailing '/' if a full segment is intended
+	`)
+
+	f.StringVar(
+		&cfg.ExchangeURL, "exchangeurl", cfg.ExchangeURL, "scheme://host:port[/path] endpoint for the token exchange. Only required for mode=reader",
+	)
+	f.StringVar(
+		&cfg.ClientID, "clientid", cfg.ClientID, "relying party client id to present to the token exchange. Only required for mode=reader",
+	)
+	f.StringVar(
+		&cfg.ClientSecret, "clientsecret", cfg.ClientSecret, "relying party client secret to present to the token exchange. Only required for mode=reader",
+	)
+
+	f.DurationVar(
+		&cfg.ShutdownGrace, "shutdown-grace", cfg.ShutdownGrace, `
+	Server shutdown gace period.
+	`)
+	f.DurationVar(
+		&cfg.WriteTimeout, "write-timout", cfg.WriteTimeout, `timeout for writes`)
+	f.DurationVar(
+		&cfg.ReadTimeout, "read-timeout", cfg.ReadTimeout, `timeout for reads`)
+	f.DurationVar(
+		&cfg.IdleTimeout, "idle-timeout", cfg.IdleTimeout, `idle timeout`)
+
+	pflag.Parse()
+
+	exitOnErr(setConfigFile(v, cfgFile, Name))
+
+	if err := v.ReadInConfig(); err != nil {
+		// It's okay if there isn't a config file
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			exitOnErr(err)
+		}
+	}
+
+	v.SetEnvPrefix(strings.ToUpper(Name))
+	v.AutomaticEnv()
+	reconcileOptions(f, v, Name)
+
+	if cfg.Mode != "reader" && cfg.Mode != "writer" {
+		exitOnErr(fmt.Errorf(
+			"ileagal mode '%s' must be either 'reader' or 'writer'", cfg.Mode))
+	}
+
+	s, err := server.NewServer(context.Background(), filepath.Dir(cfgFile), &cfg)
+	exitOnErr(err)
+
+	s.Serve()
+}
+
+func exitOnErr(err error) {
+	if err == nil {
+		return
+	}
+	fmt.Printf("error: %v\n", err)
+}
+
+func reconcileOptions(flags *pflag.FlagSet, v *viper.Viper, name string) {
+
+	envPrefix := strings.ToUpper(strings.ReplaceAll(name, ".", "_"))
+
+	flags.VisitAll(func(f *pflag.Flag) {
+
+		// Environment variables can't have dashes in them, so bind them to their equivalent
+		// keys with underscores, e.g. --favorite-color to STING_FAVORITE_COLOR
+
+		envVar := strings.ToUpper(strings.ReplaceAll(f.Name, "-", "_"))
+		if len(envPrefix) > 0 {
+			envVar = fmt.Sprintf("%s_%s", envPrefix, envVar)
+		}
+
+		fmt.Printf("bind env: %s -> %s\n", envVar, f.Name)
+		v.BindEnv(f.Name, envVar)
+
+		// Apply the viper config value to the flag when the flag is not set and viper has a value
+		if !f.Changed && v.IsSet(f.Name) {
+			val := v.Get(f.Name)
+			fmt.Println("set:", f.Name, val)
+			flags.Set(f.Name, fmt.Sprintf("%v", val))
+		}
+	})
+}
+
+func setDefaultConfig(v *viper.Viper, name string, cfg interface{}) error {
+	marshaled, err := json.Marshal(cfg)
+	if err != nil {
+		return err
+	}
+	var mapped map[string]interface{}
+	if err := json.Unmarshal(marshaled, &mapped); err != nil {
+		return err
+	}
+	v.SetDefault(name, mapped)
+	return nil
+}
+
+func setConfigFile(v *viper.Viper, cfgFile, name string) error {
+	if cfgFile != "" {
+		v.SetConfigFile(cfgFile)
+	} else {
+		home, err := os.UserHomeDir()
+		if err != nil {
+			return err
+		}
+		v.AddConfigPath(".")
+		v.AddConfigPath(home)
+		v.SetConfigName(name)
+	}
+	return nil
+}
---
+++ go/server/serve.go
@@ -0,0 +1,148 @@
+package server
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"os/signal"
+	"strings"
+	"time"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+)
+
+const (
+	ConfigName  = "server"
+	DefaultPort = 8401
+)
+
+type Config struct {
+	Mode string
+	Address       string
+	Prefix        string
+	ExchangeURL   string
+	ClientID      string
+	ClientSecret  string
+	ShutdownGrace time.Duration
+	WriteTimeout  time.Duration
+	ReadTimeout   time.Duration
+	IdleTimeout   time.Duration
+}
+
+func NewConfig() Config {
+	cfg := Config{
+		Mode: "",
+		Address:       fmt.Sprintf("0.0.0.0:%d", DefaultPort),
+		Prefix:        "",
+		ExchangeURL:   "",
+		ClientID:      "",
+		ClientSecret:  "",
+		ShutdownGrace: time.Second * 15,
+		WriteTimeout:  time.Second * 15,
+		ReadTimeout:   time.Second * 15,
+		IdleTimeout:   time.Second * 60,
+	}
+	return cfg
+}
+
+type Server struct {
+	ConfigFileDir string
+	cfg           *Config
+}
+
+type Option func(*Server)
+
+func NewServer(
+	ctx context.Context, configFileDir string, cfg *Config, opts ...Option) (Server, error) {
+
+	s := Server{
+		ConfigFileDir: configFileDir,
+		cfg:           cfg,
+	}
+
+	for _, opt := range opts {
+		opt(&s)
+	}
+	return s, nil
+}
+
+func normalisePrefixPath(prefix string) (string, error) {
+	if prefix == "" {
+		return "/", nil
+	}
+
+	// Normalise to exactly one leading '/'
+	prefix = "/" + strings.TrimLeft(prefix, "/")
+
+	u, err := url.ParseRequestURI(prefix)
+	if err != nil {
+		return "", err
+	}
+	return u.Path, nil
+}
+
+func (s *Server) Serve() {
+
+	// Add your routes as needed
+	r := mux.NewRouter()
+
+	path, err := normalisePrefixPath(s.cfg.Prefix)
+	if err != nil {
+		log.Fatalf("bad route prefix: %v", err)
+	}
+	path = fmt.Sprintf("%sexchange", path)
+	r.Handle(path, NewExchanger(s.cfg))
+
+	proxyPath, err := normalisePrefixPath(s.cfg.Prefix)
+	proxyPath = fmt.Sprintf("%sproxy", path)
+	if err != nil {
+		log.Fatalf("bad route prefix: %v", err)
+	}
+	r.PathPrefix(proxyPath).Handler(NewProxy(s.cfg))
+
+
+	logged := handlers.LoggingHandler(os.Stdout, r)
+
+	srv := &http.Server{
+		Addr: s.cfg.Address,
+		// Good practice to set timeouts to avoid Slowloris attacks.
+		WriteTimeout: s.cfg.WriteTimeout,
+		ReadTimeout:  s.cfg.ReadTimeout,
+		IdleTimeout:  s.cfg.IdleTimeout,
+		Handler:      logged, // Pass our instance of gorilla/mux in.
+		// Handler: r, // Pass our instance of gorilla/mux in.
+	}
+
+	log.Println("serving:", srv.Addr, "path:", path, "proxyPath:", proxyPath)
+
+	// Run our server in a goroutine so that it doesn't block.
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
+	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+	signal.Notify(c, os.Interrupt)
+
+	// Block until we receive our signal.
+	<-c
+
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), s.cfg.ShutdownGrace)
+	defer cancel()
+	// Doesn't block if no connections, but will otherwise wait
+	// until the timeout deadline.
+	srv.Shutdown(ctx)
+	// Optionally, you could run srv.Shutdown in a goroutine and block on
+	// <-ctx.Done() if your application should wait for other services
+	// to finalize based on context cancellation.
+	log.Println("shutting down")
+	os.Exit(0)
+}
